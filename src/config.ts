0ba98fc25201d5063e5d4f8d40754d13f8d1d6f4
James S <james.steer98gmail.com>
2024-11-17 20:59:33 +0000

    update config env

--- src/config.ts
+++ src/config.ts
@@ -2,15 +2,44 @@ import dotenv from "dotenv";
 import { FatalException } from "./lib/custom-error";
 dotenv.config();
 
-const { DISCORD_TOKEN, BRAVE_API_KEY, ENABLE_WEB_SEARCH } = process.env;
+const {
+  // Discord settings
+  DISCORD_TOKEN,
 
-if (!DISCORD_TOKEN) {
-  throw new FatalException('Missing environment variable: DISCORD_TOKEN');
-}
+  // Web search settings
+  ENABLE_WEB_SEARCH,
+  BRAVE_API_KEY,
+
+  // Ollama settings
+  ENABLE_OLLAMA_AI,
+  OLLAMA_SERVER_URL,
+  OLLAMA_SERVER_PORT,
+
+  // Coqui settings
+  ENABLE_COQUI_AI,
+  COQUI_SERVER_URL,
+  COQUI_SERVER_PORT
+} = process.env;
+
+if (DISCORD_TOKEN === 'PLACEHOLDER' || !DISCORD_TOKEN)
+  throw new FatalException('Failed to start, Discord token unset');
 
 export const ENV_CONFIG = {
+  // Discord settings
   DISCORD_TOKEN,
+
+  // Web search settings
+  ENABLE_WEB_SEARCH: ENABLE_WEB_SEARCH === 'true', // Convert to bool from string
   BRAVE_API_KEY,
-  ENABLE_WEB_SEARCH: ENABLE_WEB_SEARCH === 'true'
+
+  // Ollama settings
+  ENABLE_OLLAMA_AI: ENABLE_OLLAMA_AI === 'true', // Convert to bool from string
+  OLLAMA_SERVER_URL,
+  OLLAMA_SERVER_PORT,
+
+  // Coqui settings
+  ENABLE_COQUI_AI: ENABLE_COQUI_AI === 'true', // Convert to bool from string
+  COQUI_SERVER_URL,
+  COQUI_SERVER_PORT
 };
 
